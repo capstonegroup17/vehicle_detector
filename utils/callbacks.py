300f3128bb579d1ea00dee8f7430383a440545e0
jojojoaaa <jojo.zhou@outlook.com>
2024-10-31 17:17:08 +0000

    Merge pull request #11 from capstonegroup17/midterm_updated
    updated mid-term codes

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,72 +1,175 @@
-# Ultralytics YOLOv5 🚀, AGPL-3.0 license
-"""Callback utils."""
-
-import threading
-
+#!/usr/bin/env python
 
 class Callbacks:
-    """Handles all registered callbacks for YOLOv5 Hooks."""
+    """"
+    Handles all registered callbacks for YOLOv5 Hooks
+    """
+
+    _callbacks = {
+        'on_pretrain_routine_start': [],
+        'on_pretrain_routine_end': [],
+
+        'on_train_start': [],
+        'on_train_epoch_start': [],
+        'on_train_batch_start': [],
+        'optimizer_step': [],
+        'on_before_zero_grad': [],
+        'on_train_batch_end': [],
+        'on_train_epoch_end': [],
+
+        'on_val_start': [],
+        'on_val_batch_start': [],
+        'on_val_image_end': [],
+        'on_val_batch_end': [],
+        'on_val_end': [],
+
+        'on_fit_epoch_end': [],  # fit = train + val
+        'on_model_save': [],
+        'on_train_end': [],
+
+        'teardown': [],
+    }
 
     def __init__(self):
-        """Initializes a Callbacks object to manage registered YOLOv5 training event hooks."""
-        self._callbacks = {
-            "on_pretrain_routine_start": [],
-            "on_pretrain_routine_end": [],
-            "on_train_start": [],
-            "on_train_epoch_start": [],
-            "on_train_batch_start": [],
-            "optimizer_step": [],
-            "on_before_zero_grad": [],
-            "on_train_batch_end": [],
-            "on_train_epoch_end": [],
-            "on_val_start": [],
-            "on_val_batch_start": [],
-            "on_val_image_end": [],
-            "on_val_batch_end": [],
-            "on_val_end": [],
-            "on_fit_epoch_end": [],  # fit = train + val
-            "on_model_save": [],
-            "on_train_end": [],
-            "on_params_update": [],
-            "teardown": [],
-        }
-        self.stop_training = False  # set True to interrupt training
+        return
 
-    def register_action(self, hook, name="", callback=None):
+    def register_action(self, hook, name='', callback=None):
         """
-        Register a new action to a callback hook.
+        Register a new action to a callback hook
 
         Args:
-            hook: The callback hook name to register the action to
-            name: The name of the action for later reference
-            callback: The callback to fire
+            hook        The callback hook name to register the action to
+            name        The name of the action
+            callback    The callback to fire
         """
         assert hook in self._callbacks, f"hook '{hook}' not found in callbacks {self._callbacks}"
         assert callable(callback), f"callback '{callback}' is not callable"
-        self._callbacks[hook].append({"name": name, "callback": callback})
+        self._callbacks[hook].append({'name': name, 'callback': callback})
 
     def get_registered_actions(self, hook=None):
-        """
-        Returns all the registered actions by callback hook.
+        """"
+        Returns all the registered actions by callback hook
 
         Args:
-            hook: The name of the hook to check, defaults to all
+            hook The name of the hook to check, defaults to all
         """
-        return self._callbacks[hook] if hook else self._callbacks
+        if hook:
+            return self._callbacks[hook]
+        else:
+            return self._callbacks
 
-    def run(self, hook, *args, thread=False, **kwargs):
+    def run_callbacks(self, hook, *args, **kwargs):
         """
-        Loop through the registered actions and fire all callbacks on main thread.
-
-        Args:
-            hook: The name of the hook to check, defaults to all
-            args: Arguments to receive from YOLOv5
-            thread: (boolean) Run callbacks in daemon thread
-            kwargs: Keyword Arguments to receive from YOLOv5
+        Loop through the registered actions and fire all callbacks
         """
-        assert hook in self._callbacks, f"hook '{hook}' not found in callbacks {self._callbacks}"
         for logger in self._callbacks[hook]:
-            if thread:
-                threading.Thread(target=logger["callback"], args=args, kwargs=kwargs, daemon=True).start()
-            else:
-                logger["callback"](*args, **kwargs)
+            # print(f"Running callbacks.{logger['callback'].__name__}()")
+            logger['callback'](*args, **kwargs)
+
+    def on_pretrain_routine_start(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the start of each pretraining routine
+        """
+        self.run_callbacks('on_pretrain_routine_start', *args, **kwargs)
+
+    def on_pretrain_routine_end(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the end of each pretraining routine
+        """
+        self.run_callbacks('on_pretrain_routine_end', *args, **kwargs)
+
+    def on_train_start(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the start of each training
+        """
+        self.run_callbacks('on_train_start', *args, **kwargs)
+
+    def on_train_epoch_start(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the start of each training epoch
+        """
+        self.run_callbacks('on_train_epoch_start', *args, **kwargs)
+
+    def on_train_batch_start(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the start of each training batch
+        """
+        self.run_callbacks('on_train_batch_start', *args, **kwargs)
+
+    def optimizer_step(self, *args, **kwargs):
+        """
+        Fires all registered callbacks on each optimizer step
+        """
+        self.run_callbacks('optimizer_step', *args, **kwargs)
+
+    def on_before_zero_grad(self, *args, **kwargs):
+        """
+        Fires all registered callbacks before zero grad
+        """
+        self.run_callbacks('on_before_zero_grad', *args, **kwargs)
+
+    def on_train_batch_end(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the end of each training batch
+        """
+        self.run_callbacks('on_train_batch_end', *args, **kwargs)
+
+    def on_train_epoch_end(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the end of each training epoch
+        """
+        self.run_callbacks('on_train_epoch_end', *args, **kwargs)
+
+    def on_val_start(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the start of the validation
+        """
+        self.run_callbacks('on_val_start', *args, **kwargs)
+
+    def on_val_batch_start(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the start of each validation batch
+        """
+        self.run_callbacks('on_val_batch_start', *args, **kwargs)
+
+    def on_val_image_end(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the end of each val image
+        """
+        self.run_callbacks('on_val_image_end', *args, **kwargs)
+
+    def on_val_batch_end(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the end of each validation batch
+        """
+        self.run_callbacks('on_val_batch_end', *args, **kwargs)
+
+    def on_val_end(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the end of the validation
+        """
+        self.run_callbacks('on_val_end', *args, **kwargs)
+
+    def on_fit_epoch_end(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the end of each fit (train+val) epoch
+        """
+        self.run_callbacks('on_fit_epoch_end', *args, **kwargs)
+
+    def on_model_save(self, *args, **kwargs):
+        """
+        Fires all registered callbacks after each model save
+        """
+        self.run_callbacks('on_model_save', *args, **kwargs)
+
+    def on_train_end(self, *args, **kwargs):
+        """
+        Fires all registered callbacks at the end of training
+        """
+        self.run_callbacks('on_train_end', *args, **kwargs)
+
+    def teardown(self, *args, **kwargs):
+        """
+        Fires all registered callbacks before teardown
+        """
+        self.run_callbacks('teardown', *args, **kwargs)
